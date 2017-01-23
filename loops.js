8e8752c44385e88f15f827c15781786ee7f2ca91
Jesus Vega <chuyspc@gmail.com>
2017-01-23 13:28:26 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,22 +1,28 @@
 function forLoop(array) {
-  for(let i = 0; i < 25; i++) {
-    if (array[i] === 1) {
-      array[i] = "I am 1 strange loop.";
-    } else {
-      array[i] = `I am ${i} strange loops.`;
+    for (let i = 0; i < 25; i++) {
+        if (i == 1) {
+            array.push("I am 1 strange loop.");
+        } else {
+            array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
+        }
     }
-  }
-  return array;
+    return array;
 }
 
 function whileLoop(n) {
-  while(n > 0) {
-    console.log(n);
-    n--;
-  }
-  return "done";
+    while (n > 0) {
+        console.log(n);
+        n--;
+    }
+    return "done";
 }
 
 function doWhileLoop(array) {
-
+    function maybeTrue() {
+        return Math.random() >= 0.5
+    }
+    do {
+        array.pop();
+    } while (array.length > 0 && maybeTrue());
+    return array;
 }
